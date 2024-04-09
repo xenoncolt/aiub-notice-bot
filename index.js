7e90bb99e374d33b4a1145cce733fec2868afb48
Md. Shahriar Haque <81591350+xenoncolt@users.noreply.github.com>
2024-04-09 21:03:04 +0600

    Update .gitignore to include package-lock.json and config.json Add example.config.json with sample configuration Update index.js to use ES6 import syntax and add functionality to fetch and send notice Update package.json to include required dependencies

--- index.js
+++ index.js
@@ -1,17 +1,54 @@
-const discord = require('discord.js');
-const dotenv = require('dotenv');
+import { Client, GatewayIntentBits, EmbedBuilder } from "discord.js";
+import config from "./config.json" assert { type: "json" };
+// const fetch = require("node-fetch");
+import fetch from "node-fetch";
+// const jsdom = require("jsdom");
+// const { JSDOM } = jsdom;
+import { JSDOM } from "jsdom";
+// const dotenv = require("dotenv");
+import dotenv from "dotenv";
 
-
-const client = new discord.Client({
+const client = new Client({
     intents: [
-        discord.GatewayIntentBits.Guilds
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent
     ]
 });
 
-client.once(discord.Events.ClientReady, bot => {
-    console.log(`Logged in as ${bot.user.tag}`);
+client.once("ready", () => {
+    console.log(`${client.user.tag} Bot is ready!`);
+    fetchNotice();
 });
 
+async function fetchNotice() {
+    try {
+        const response = await fetch(config.notice_url);
+        const text = await response.text();
+        const dom = new JSDOM(text);
+        const document = dom.window.document;
+
+        const title = document.querySelector('.title').textContent;
+        const desc = document.querySelector('.desc').textContent;
+        const link_info = document.querySelector('.info-link').href;
+
+        const link = `${config.url}${link_info}`;
+
+        const channel = client.channels.cache.get(config.channel_id);
+
+        if (channel) {
+            const embed = new EmbedBuilder()
+                .setTitle(title)
+                .setDescription(desc)
+                .setURL(link)
+                .setColor("Green")
+                .setTimestamp();
+            channel.send( { embeds: [embed]});
+        }
+    } catch (error) {
+        console.error('Failed to catch notice: ',error);
+    }
+}
 
 dotenv.config();
 client.login(process.env.TOKEN);
