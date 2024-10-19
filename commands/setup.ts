f2ca67960242735737aac55e459068b9269fd431
Xenon Colt <s.h.shahriar.haque@gmail.com>
2024-10-19 19:49:28 +0600

    fix: can not setup channel

--- commands/setup.ts
+++ commands/setup.ts
@@ -2,6 +2,7 @@ import sqlite3 from "sqlite3";
 import { Database, open } from "sqlite";
 import { ChatInputCommandInteraction, Client, GuildMember, NewsChannel, PermissionFlagsBits, TextChannel } from "discord.js";
 import { Command } from "../types/Command";
+import { channelDB } from "../schema/aiubNews";
 
 let db: Database;
 (async () => {
@@ -19,6 +20,8 @@ let db: Database;
     `);
 })();
 
+const channel_db = channelDB();
+
 export default {
     name: 'setup',
     description: 'Setup or reset a channel',
@@ -26,13 +29,24 @@ export default {
         {
             name: 'notice',
             type: 1,
-            description: 'Setup a channel',
+            description: 'Setup a channel for send new notice',
             options: [{
                 name: 'channel',
                 type: 7,
                 description: 'Select a channel to setup',
                 required: true,
             }],
+        },
+        {
+            name: 'news',
+            type: 1,
+            description: 'Setup a news for send new News and Events',
+            options: [{
+                name: 'channel',
+                type: 7,
+                description: 'Select a channel to setup',
+                required: true,
+            }]
         },
         {
             name: 'reset',
@@ -70,7 +84,7 @@ export default {
 
             if (sub_cmd === 'notice') {
                 if (!member.permissions.has([PermissionFlagsBits.Administrator, PermissionFlagsBits.ManageChannels])) {
-                    await interaction.editReply("You don't have enough permission to set up a notice channel.");
+                    await interaction.editReply("You don't have enough permission to set up a **Notice** channel.");
                     return;
                 }
 
@@ -80,7 +94,7 @@ export default {
                 }
 
                 if (!permission.has([PermissionFlagsBits.ViewChannel, PermissionFlagsBits.ManageRoles])) {
-                    await interaction.editReply('I don\'t have permission to view or manage role in this channel. If you don\'t know how to give me that permission then just invite me again (Click to my profile -> Add App -> Add to server).');
+                    await interaction.editReply(`I don\'t have permission to view or manage role in ${channel.id} channel. If you don\'t know how to give me that permission then just invite me again (Click to my profile -> Add App -> Add to server).`);
                     return;
                 }
 
@@ -92,6 +106,31 @@ export default {
                 await db.run('INSERT OR IGNORE INTO channel (guild_id, channel_id) VALUES (?, ?)', [guild_id, channel.id]);
                 await interaction.editReply('Channel has been set! <:Nice:791390203944239134> Please wait for the next notice.\nPlease review me here. <:crying_praying:791390109839654922> \n[Review](https://top.gg/bot/1123156043711651910#reviews)');
                 await channel.send({ content: `New Notice will be post here.\nPlease wait for that.\nDon't delete this channel. Deleting this channel means stop posting notice in this channel.\nDon't change any permission of this channel. Change when you have knowledge about channel management.` });
+            } else if (sub_cmd === 'news') {
+                if (!member.permissions.has([PermissionFlagsBits.Administrator, PermissionFlagsBits.ManageChannels])) {
+                    await interaction.editReply("You don't have enough permission to set up **News & Events** channel.");
+                    return;
+                }
+
+                if (!(channel instanceof TextChannel) && !(channel instanceof NewsChannel)) {
+                    await interaction.editReply('Channel is not a Text Channel Or Announcement Channel. Please select a text channel or announcement channel.');
+                    return;
+                }
+
+                if (!permission.has([PermissionFlagsBits.ViewChannel, PermissionFlagsBits.ManageRoles])) {
+                    await interaction.editReply(`I don't have permission to view or manage role in ${channel.id} channel. If you don\'t know how to give me that permission then just invite me again (Click to my profile -> Add App -> Add to server).`);
+                    return;
+                }
+
+                if (permission.has([PermissionFlagsBits.ViewChannel, PermissionFlagsBits.ManageChannels, PermissionFlagsBits.ManageRoles])) {
+                    await channel.permissionOverwrites.create(client.user!, { SendMessages: true, EmbedLinks: true });
+                    await channel.permissionOverwrites.create(channel.guild.roles.everyone, { SendMessages: false });
+                }
+
+                (await channel_db).run('INSERT OR IGNORE INTO aiubNewsChannel (guild_id, channel_id) VALUES (?, ?)', guild_id, channel.id);
+                await interaction.editReply('Channel has been set! <:Nice:791390203944239134> Please wait for the next AIUB News & Events.\nPlease review me here. <:crying_praying:791390109839654922> \n[Review](https://top.gg/bot/1123156043711651910#reviews)');
+                await channel.send({ content: `New AIUB News & Events will be post here.\nPlease wait for that.\nDon't delete this channel. Deleting this channel means stop posting AIUB News & Events in this channel.\nDon't change any permission of this channel. Change when you have knowledge about channel management.` });
+                
             } else if (sub_cmd === 'reset') {
                 if (!member.permissions.has([PermissionFlagsBits.Administrator, PermissionFlagsBits.ManageChannels])) {
                     await interaction.editReply("You don\'t have enough permission to setup notice channel.");
