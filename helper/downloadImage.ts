c43da8170b35053a4eb012059479818abbb86369
Xenon Colt <s.h.shahriar.haque@gmail.com>
2025-08-01 17:30:35 +0600

    r u serious.. aura--

--- helper/downloadImage.ts
+++ helper/downloadImage.ts
@@ -6,25 +6,40 @@ import { fileURLToPath } from "url";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-export async function downloadImage(urls: string[]): Promise<Buffer[]> {
+export async function downloadImage(urls: string[] | string): Promise<Buffer[] | Buffer> {
     const dir = join(__dirname, '../download');
     let buffers: Buffer[] = [];
 
     mkdirSync(dir, { recursive: true });
 
-    for (const [index, url] of urls.entries()) {
-        const img_path = join(dir, `temp-${index}.jpg`);
+    if (urls instanceof Array) {
+        for (const [index, url] of urls.entries()) {
+            const img_path = join(dir, `temp-${index}.jpg`);
+            const writer = createWriteStream(img_path);
+            const res = await axios({
+                url,
+                method: 'GET',
+                responseType: 'stream'
+            });
+            res.data.pipe(writer);
+            buffers.push(await new Promise<Buffer>((resolve, reject) => {
+                writer.on('finish', () => resolve(Buffer.from(img_path)));
+                writer.on('error', reject);
+            }));
+        }
+    } else {
+        const img_path = join(dir, 'temp.jpg');
         const writer = createWriteStream(img_path);
         const res = await axios({
-            url,
+            url: urls,
             method: 'GET',
             responseType: 'stream'
         });
         res.data.pipe(writer);
-        buffers.push(await new Promise<Buffer>((resolve, reject) => {
+        return new Promise<Buffer>((resolve, reject) => {
             writer.on('finish', () => resolve(Buffer.from(img_path)));
             writer.on('error', reject);
-        }));
+        });
     }
 
     //const img_path = join(dir, 'temp.jpg');
